e341aa6a76ea80764771d6415a1091d62353992f
Delan Azabani <dazabani@igalia.com>
2024-04-09 15:15:07 +0800

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,7 @@
+with import (builtins.fetchTarball {
+  url = "https://github.com/NixOS/nixpkgs/archive/b06025f1533a1e07b6db3e75151caa155d1c7eb3.tar.gz";
+}) {};
+stdenv.mkDerivation rec {
+  name = "servo-book-env";
+  buildInputs = [ mdbook ];
+}
